5f88a2df3f93681daa3a29928fd9d7bc65c82be8
Karimboyev0509 <karimboyevmuhammadkarim@gmail.com>
2025-10-06 23:07:06 +0500

    Update dars1.sql

--- lesson-1/homework/dars1.sql
+++ lesson-1/homework/dars1.sql
@@ -1,2 +1,7 @@
 Easy
 1.
+  Data: Data means raw facts or information that can be stored, processed and analyzed.
+  Definition: A database is an organized collection of related data stored in a structured way so it can be easily accessed, managed, and updated.
+  Relational Database: A relational database is a type of database that stores data in tables with rows and columns. Each table is related to other through keys.
+  Table: A table is a structure inside a database that stores data in rows and columns.
+  
