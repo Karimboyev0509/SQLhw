0cabfcc1733daabd254d12607d1dec7774b93cfd
Karimboyev0509 <karimboyevmuhammadkarim@gmail.com>
2025-10-07 14:44:26 +0500

    Update dars1.sql

--- lesson-1/homework/dars1.sql
+++ lesson-1/homework/dars1.sql
@@ -5,6 +5,14 @@ Definition: A database is an organized collection of related data stored in a st
 Relational Database: A relational database is a type of database that stores data in tables with rows and columns. Each table is related to other through keys.
 Table: A table is a structure inside a database that stores data in rows and columns.
 2.
+1) High performance- Sql Server can handle large amounts of data quickly and efficiently.
+2) Security- it provides authentication, encryption and role-based access control to protect data.
+3) Data recovery- supports backup and restore to prevent data loss.
+4) Scalability – can manage small databases or very large enterprise systems.
+5) Integration – works well with other Microsoft tools like Power BI, Excel, and Azure.
+3.
+
+
 
 
 
